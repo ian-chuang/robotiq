8db0f5c216dd2d45c1f3b8ab7dd1d095da91576d
Ian Chuang <iantc104@gmail.com>
2023-02-02 20:08:03 -0800

    added error handling to ModbusRTU Control

--- robotiq_modbus_rtu/src/robotiq_modbus_rtu/comModbusRtu.py
+++ robotiq_modbus_rtu/src/robotiq_modbus_rtu/comModbusRtu.py
@@ -48,7 +48,13 @@ from __future__ import print_function
 from pymodbus.client.sync import ModbusSerialClient
 from math import ceil
 
-class communication:
+class ReadGripperError(Exception):
+   pass
+
+class WriteGripperError(Exception):
+   pass
+
+class ComModbusRtu:
 
    def __init__(self):
       self.client = None
@@ -79,23 +85,29 @@ class communication:
          message.append((data[2*i] << 8) + data[2*i+1])
 
       #To do!: Implement try/except
-      self.client.write_registers(0x03E8, message, unit=0x0009)
-
+      try:
+         self.client.write_registers(0x03E8, message, unit=0x0009)
+      except:
+         raise WriteGripperError("Failed to write to registers of gripper")
+         
    def getStatus(self, numBytes):
       """Sends a request to read, wait for the response and returns the Gripper status. The method gets the number of bytes to read as an argument"""
       numRegs = int(ceil(numBytes/2.0))
 
       #To do!: Implement try/except
       #Get status from the device
-      response = self.client.read_holding_registers(0x07D0, numRegs, unit=0x0009)
+      try:
+         response = self.client.read_holding_registers(0x07D0, numRegs, unit=0x0009)
 
-      #Instantiate output as an empty list
-      output = []
+         #Instantiate output as an empty list
+         output = []
 
-      #Fill the output with the bytes in the appropriate order
-      for i in range(0, numRegs):
-         output.append((response.getRegister(i) & 0xFF00) >> 8)
-         output.append( response.getRegister(i) & 0x00FF)
+         #Fill the output with the bytes in the appropriate order
+         for i in range(0, numRegs):
+            output.append((response.getRegister(i) & 0xFF00) >> 8)
+            output.append( response.getRegister(i) & 0x00FF)
+      except:
+         raise ReadGripperError("Failed to get registers from gripper")
 
       #Output the result
       return output
